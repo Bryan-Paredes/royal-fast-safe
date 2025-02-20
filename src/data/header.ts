dfb21c5be33633c1c1d9d90245c74170e54fc312
BryanParedes <bryansparedes2@gmail.com>
2025-02-19 20:03:39 -0600

    Adding routes and services

---
+++ src/data/header.ts
@@ -0,0 +1,50 @@
+export const routes: { title: string; href: string }[] = [
+    {
+        title: "About",
+        href: "#about",
+    },
+    {
+        title: "Blog",
+        href: "/blog",
+    },
+
+]
+
+export const services: { title: string; href: string; description: string }[] = [
+    {
+        title: "Heavy Equipment",
+        href: "/service/heavy-equipment",
+        description:
+            "From Freight of nearly any shape, size or weight to Agricultural and Heavy Equipment as well as Oversize Loads. Just tell us what you need to ship and we will make it happen!",
+    },
+    {
+        title: "Container",
+        href: "/service/container",
+        description:
+            "We can provides custom container shipping solutions to fit your needs. Toyal Fast and Safe is one of the industry's best shipping contaiiner mover companies.",
+    },
+    {
+        title: "Power Only",
+        href: "/service/power-only",
+        description:
+            "If you need Power Only services, we will use our Semi Trucks to low your trailers.",
+    },
+    {
+        title: "Boat",
+        href: "/service/boat",
+        description:
+            "Small or large boat, we treat your boat as if it's our own! Licensed to transport your vessel Fast and Safe.",
+    },
+    {
+        title: "RV / 5th Wheel Shipping",
+        href: "/service/rv-5th-wheel-shipping",
+        description:
+            "RV & Travel Trailer, we can carry the weight, ramps, center reel and winch, All equipment for the haul.",
+    },
+    {
+        title: "Complicated Logistics",
+        href: "/service/complicated-logistics",
+        description:
+            "Whatever yout heavy equipment needs are, we've got the haulng capability. Complicated Logistics are our specialty. All you have yo do is let us know what your shipping goals are, and we will work out the most efficient way to get your cargo where it needs to be.",
+    },
+];
