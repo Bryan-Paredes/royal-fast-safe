de087a753dbabbe708ee2f07affe86b534c4808b
BryanParedes <bryansparedes2@gmail.com>
2025-02-19 20:12:38 -0600

    Update initial theme state in ModeToggle component to 'theme-light'

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -11,7 +11,7 @@ import {
 
 export function ModeToggle() {
   const [theme, setThemeState] = useState<"theme-light" | "dark" | "system">(
-    "system"
+    "theme-light"
   );
 
   useEffect(() => {
