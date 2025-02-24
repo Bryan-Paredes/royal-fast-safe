f8a94423d52cf3dbb6e8046f11a264be9b3df28b
BryanParedes <bryansparedes2@gmail.com>
2025-02-24 17:15:20 -0600

    Agregar datos para el carrusel de imágenes y las banderas; actualizar la interfaz del logoGrid y añadir un hook para clics fuera del componente

---
+++ src/data/carousel.ts
@@ -0,0 +1,22 @@
+export const images = [
+    {
+        id: 1,
+        src: "/slideImages/Grovecrane.webp",
+    },
+    {
+        id: 2,
+        src: "/slideImages/mathsmti.webp",
+    },
+    {
+        id: 3,
+        src: "/slideImages/13Items.webp",
+    },
+    {
+        id: 4,
+        src: "/slideImages/Loaded.webp",
+    },
+    {
+        id: 5,
+        src: "/slideImages/vantruck.webp",
+    }
+]
--- src/data/logoGrid.ts
+++ src/data/logoGrid.ts
@@ -2,7 +2,6 @@ export interface LogoGrid {
     id: number;
     name: string;
     logo: string;
-    link: string;
 }
 
 export const logosGrid: LogoGrid[] = [
@@ -10,48 +9,48 @@ export const logosGrid: LogoGrid[] = [
         id: 1,
         name: "Uship",
         logo: "/logosGrid/uship-logo.webp",
-        link: "/",
+
     },
     {
         id: 2,
         name: "FR8 Star",
         logo: "/logosGrid/fr8-star.webp",
-        link: "/",
+
     },
     {
         id: 3,
         name: "Veritread",
         logo: "/logosGrid/veritread.webp",
-        link: "/",
+
     },
     {
         id: 4,
         name: "Equipment Facts",
         logo: "/logosGrid/equipment-facts.webp",
-        link: "/",
+
     },
     {
         id: 5,
         name: "Iron Planet",
         logo: "/logosGrid/iron-planet.webp",
-        link: "/",
+
     },
     {
         id: 6,
         name: "Gov Planet",
         logo: "/logosGrid/gov-planet.webp",
-        link: "/",
+
     },
     {
         id: 7,
         name: "Ritchie Bros",
         logo: "/logosGrid/ritchie-bros.webp",
-        link: "/",
+
     },
     {
         id: 8,
         name: "Sam Gov",
         logo: "/logosGrid/sam-gov.webp",
-        link: "/",
+
     },
 ]
