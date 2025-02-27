a68c1f69d51db7767962c266ecc92df420c58cdc
BryanParedes <bryansparedes2@gmail.com>
2025-02-26 20:38:00 -0600

    Add Metrics, Services and Timeline sections; update the Experience section and modify the header and footer layout

--- src/sections/NavBar.tsx
+++ src/sections/NavBar.tsx
@@ -11,7 +11,7 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { Button } from "@/components/ui/button";
-import { Mail, MenuIcon, PhoneCall } from "lucide-react";
+import { Mail, PanelRightClose, PhoneCall } from "lucide-react";
 import { ModeToggle } from "@/components/ModeToggle";
 import {
   Sheet,
@@ -25,19 +25,33 @@ import { routes, services } from "@/data/header";
 
 export default function NavBar() {
   return (
-    <nav className="container flex items-center justify-between w-full max-w-6xl mx-auto px-6 lg:px-0 p-4 border-b border-primary-300 ">
-      <div className="flex items-center justify-center">
-        <a href="/">
+    <nav className="flex fixed items-center justify-evenly z-50 w-full mx-auto px-4">
+      <div className="flex w-full mx-auto items-center justify-between scroll-auto backdrop-blur-sm backdrop-filter bg-background max-w-7xl mt-1 rounded-xl px-5 shadow-lg dark:shadow-lg dark:shadow-gray-400/30">
+        <a href="/" className="block md:hidden">
           <img
             src="/royal.webp"
             alt="Logo"
-            className="h-20 w-20 object-cover"
+            className="h-20 w-20 object-cover object-center"
+            loading="eager"
             decoding="async"
+            width={100}
+            height={100}
           />
         </a>
         <NavigationMenu>
           <NavigationMenuList className="hidden md:flex">
             <NavigationMenuItem>
+              <NavigationMenuLink href="/">
+                <img
+                  src="/royal.webp"
+                  alt="Logo"
+                  className="h-20 w-20 object-cover object-center"
+                  loading="eager"
+                  decoding="async"
+                />
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem className="text-lg">
               <NavigationMenuLink
                 className={`${navigationMenuTriggerStyle()} hover:text-white`}
                 href="/about"
@@ -45,9 +59,9 @@ export default function NavBar() {
                 About
               </NavigationMenuLink>
             </NavigationMenuItem>
-            <NavigationMenuItem>
+            <NavigationMenuItem className="text-lg">
               <NavigationMenuTrigger className="hover:text-white">
-                Components
+                Services
               </NavigationMenuTrigger>
               <NavigationMenuContent>
                 <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
@@ -69,76 +83,80 @@ export default function NavBar() {
                 <NavigationMenuLink
                   className={`${navigationMenuTriggerStyle()} hover:text-white`}
                 >
-                  Blog
+                  Newsroom
                 </NavigationMenuLink>
               </a>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
-      </div>
-      <div className="flex items-center space-x-2">
-        <ModeToggle />
-        <Button
-          variant="outline"
-          size="icon"
-          className="rounded-full hover:text-white"
-        >
-          <a href="/">
-            <PhoneCall size={20} />
-          </a>
-        </Button>
-        <Button
-          variant="default"
-          size="default"
-          asChild
-          className="hidden md:flex uppercase items-center gap-2 px-4 py-2 rounded-lg bg-primary-400 hover:bg-primary-600 transition-colors duration-200 ease-in-out text-white/80"
-        >
-          <a href="/contact" className="">
-            <span className="font-medium uppercase">Contact</span>
-            <Mail size={18} strokeWidth={2} className="text-white/80" />
-          </a>
-        </Button>
-        <Sheet>
-          <SheetTrigger className="flex md:hidden rounded-2xl">
-            <Button variant="outline" size="icon" className="rounded-full">
-              <MenuIcon size={24} />
-            </Button>
-          </SheetTrigger>
-          <SheetContent>
-            <SheetHeader>
-              <SheetTitle className="text-left">Routes</SheetTitle>
-              <hr className="border-primary-300" />
-            </SheetHeader>
-            <ul className="flex flex-col gap-3 p-2">
-              {routes.map((route) => (
-                <li key={route.title}>
-                  <a href={route.href}>{route.title}</a>
-                </li>
-              ))}
-            </ul>
-            <SheetHeader className="my-3">
-              <SheetTitle className="text-left">Services</SheetTitle>
-              <hr className="border-primary-300" />
-            </SheetHeader>
-            <ul className="flex flex-col gap-3 p-2 ">
-              {services.map((service) => (
-                <li key={service.title}>
-                  <a href={service.href}>{service.title}</a>
-                </li>
-              ))}
-            </ul>
-            <Button
-              variant="outline"
-              size="lg"
-              className="uppercase stroke-2 items-center gap-2 px-4 py-2 rounded-lg bg-primary-400 hover:bg-primary-600 hover:text-white transition-colors duration-200 ease-in-out text-white/80 mx-auto mt-3"
-            >
-              <a href="/contact" className="flex items-center gap-2">
-                <span className="font-medium">Contact</span>
-                <Mail size={18} className="text-white/80" />
-              </a>
-            </Button>
-          </SheetContent>
-        </Sheet>
+        <div className="flex items-center justify-end space-x-2">
+          <ModeToggle />
+          <Button
+            variant="outline"
+            size="icon"
+            className="rounded-full hover:text-white"
+          >
+            <a href="/">
+              <PhoneCall size={20} />
+            </a>
+          </Button>
+          <Button
+            variant="default"
+            size="default"
+            asChild
+            className="hidden md:flex uppercase items-center gap-2 px-4 py-2 rounded-lg bg-primary-400 hover:bg-primary-600 transition-colors duration-200 ease-in-out text-white/80"
+          >
+            <a href="/contact" className="">
+              <span className="font-medium uppercase">Contact</span>
+              <Mail size={18} strokeWidth={2} className="text-white/80" />
+            </a>
+          </Button>
+          <Sheet>
+            <SheetTrigger className="flex md:hidden rounded-2xl">
+              <Button
+                variant="outline"
+                size="icon"
+                className="rounded-full hover:text-white cursor-pointer"
+              >
+                <PanelRightClose size={24} />
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle className="text-left">Routes</SheetTitle>
+                <hr className="border-primary-300" />
+              </SheetHeader>
+              <ul className="flex flex-col gap-3 p-2">
+                {routes.map((route) => (
+                  <li key={route.title}>
+                    <a href={route.href}>{route.title}</a>
+                  </li>
+                ))}
+              </ul>
+              <SheetHeader className="my-3">
+                <SheetTitle className="text-left">Services</SheetTitle>
+                <hr className="border-primary-300" />
+              </SheetHeader>
+              <ul className="flex flex-col gap-3 p-2 ">
+                {services.map((service) => (
+                  <li key={service.title}>
+                    <a href={service.href}>{service.title}</a>
+                  </li>
+                ))}
+              </ul>
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase stroke-2 items-center gap-2 px-4 py-2 rounded-lg bg-primary-400 hover:bg-primary-600 hover:text-white transition-colors duration-200 ease-in-out text-white/80 mx-auto mt-3"
+              >
+                <a href="/contact" className="flex items-center gap-2">
+                  <span className="font-medium">Contact</span>
+                  <Mail size={18} className="text-white/80" />
+                </a>
+              </Button>
+            </SheetContent>
+          </Sheet>
+        </div>
       </div>
     </nav>
   );
