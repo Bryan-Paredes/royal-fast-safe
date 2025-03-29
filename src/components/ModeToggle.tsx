42ccb402de66763c9e42a71d91789d8ecc6c89c9
Bryan Paredes <bryansparedes2@gmail.com>
2025-03-28 18:16:49 -0600

    Change Styles

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -33,7 +33,7 @@ export function ModeToggle() {
         <Button
           variant="outline"
           size="default"
-          className="bg-background border-none rounded-full hover:text-white transition-colors duration-100 ease-in-out cursor-pointer"
+          className="bg-background border-none rounded-full hover:text-white hover:bg-primary-600 transition-colors duration-100 ease-in-out cursor-pointer"
         >
           <Sun className="h-[2rem] w-[2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 " />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
